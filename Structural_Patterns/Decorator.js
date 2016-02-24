9cb636c70fc091f6b0b906a680fe52764cc0b8c2
Victor P. Almeida <victor.stf@gmail.com>
2016-02-24 11:37:58 -0300

    design pattern

---
+++ Structural_Patterns/Decorator.js
@@ -0,0 +1,43 @@
+var User = function(name) {
+    this.name = name;
+ 
+    this.say = function() {
+        log.add("User: " + this.name);
+    };
+}
+ 
+var DecoratedUser = function(user, street, city) {
+    this.user = user;
+    this.name = user.name;  // ensures interface stays the same
+    this.street = street;
+    this.city = city;
+ 
+    this.say = function() {
+        log.add("Decorated User: " + this.name + ", " +
+                   this.street + ", " + this.city);
+    };
+}
+ 
+// logging helper
+ 
+var log = (function() {
+    var log = "";
+ 
+    return {
+        add: function(msg) { log += msg + "\n"; },
+        show: function() { console.log(log); log = ""; }
+    }
+})();
+ 
+function run() {
+ 
+    var user = new User("Kelly");
+    user.say();
+ 
+    var decorated = new DecoratedUser(user, "Broadway", "New York");
+    decorated.say();
+ 
+    log.show();
+}
+
+run();
